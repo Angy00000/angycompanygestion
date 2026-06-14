b15972ad82ff615ae2d102ace6af25a110b34fd9
Angy00000 <angejacques99@gmail.com>
2026-06-14 22:50:54 +0000

    fix: Clear localStorage catalogue au démarrage dans main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// Force reset catalogue si version outdatée
+localStorage.removeItem("angy_catalogue");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
